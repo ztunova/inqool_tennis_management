86b34f9ab087b136f4739ffcfe9c2d2e9f63fc38
Zofia Tunova <553677@mail.muni.cz>
2024-06-03 16:45:22 +0200

    add referencing fields

--- src/main/java/com/example/inqool_task/data/model/Customer.java
+++ src/main/java/com/example/inqool_task/data/model/Customer.java
@@ -2,15 +2,18 @@ package com.example.inqool_task.data.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -23,18 +26,34 @@ public class Customer {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "phone_number", unique = true, nullable = false)
     private String phoneNumber;
 
+    @OneToMany(mappedBy = "customer",
+            fetch = FetchType.EAGER
+    )
+    private Set<Reservation> reservations;
+
+
+    public void addReservation(Reservation reservation) {
+        this.reservations.add(reservation);
+    }
+
+    public boolean removeReservation(Reservation reservation) {
+        return reservations.remove(reservation);
+    }
+
+
     @Override
     public String toString() {
-        return "User{" +
+        return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", phone number='" + phoneNumber + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", reservations=" + reservations.stream().map(r -> " " + r.getId()) +
                 '}';
     }
 
